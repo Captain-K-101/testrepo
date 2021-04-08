19f4b7beb1a67cc083ea2ee283bc09625b9635aa
Nikhil <jasonnair83@gmail.com>
2021-04-08 13:22:11 +0530

    Added changes

---
+++ Projects/index.js
@@ -0,0 +1,14 @@
+{
+    "data":[
+        {
+            "Name":"Ssrf-Lab's",
+            "Link":"https://github.com/Captain-K-101/Ssrf-labs",
+            "Description":"Server-Side Request Forgery (SSRF) vulnerable Lab. This has a multitude of ssrf challenges that i have encountered while playing ctfs, ranging from beginner to medium level"
+        },
+        {
+            "Name":"God's Eye",
+            "Link":"https://github.com/Captain-K-101/Gods_Eye",
+            "Description":"Tool In Development used for Ctf and bug bounty recon's for low hanging fruits like sql/xss parameteres subdomains and spf and dmark checks"
+        }
+    ]
+}
